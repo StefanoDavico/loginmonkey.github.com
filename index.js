7fa73373d2d58cc598bfc53b088777d7d495d99c
StefanoDavico <117077889+StefanoDavico@users.noreply.github.com>
2023-02-11 22:50:34 +0100

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", function(req, res) {
-  res.sendFile("./Sito/index.html");
+  res.send("Funziona");
 });
 
 app.listen(3000, function() {
