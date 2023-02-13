69e9de5efa2f6d501e86f6949ede9eda819f6274
StefanoDavico <117077889+StefanoDavico@users.noreply.github.com>
2023-02-13 15:36:01 +0100

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const path = require('path');
 const router = express.Router();
  
 router.get('/',function(req,res){
-  res.sendFile(__dirname+'/index.html');
+  res.sendFile(__dirname+'./index.html');
   //__dirname : It will resolve to your project folder.
 });
  
